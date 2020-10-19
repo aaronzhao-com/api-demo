e9dc2c8cf5e747af5c365eff51cb847a5569daf6
Aaron Zhao <aaron.zhao@myob.com>
2020-10-20 10:07:39 +1100

    added description

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 module.exports.appName = process.env.APP_NAME || 'myapplication';
 module.exports.version = process.env.APP_VERSION || '';
 module.exports.lastCommitSha = process.env.APP_COMMIT || '';
-module.exports.description = process.env.DESCRIPTION || '';
+module.exports.description = process.env.DESCRIPTION || 'pre-interview technical test';
 module.exports.port = process.env.NODE_ENV === 'test' ? 3000 : 8080;
