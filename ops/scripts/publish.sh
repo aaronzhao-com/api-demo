c925d4ac1802bd56ff6567f02f26613c10f93055
Aaron Zhao <aaron.zhao@myob.com>
2020-10-19 16:08:23 +1100

    aws key profile because I have multiple profiles on my laptop

--- ops/scripts/publish.sh
+++ ops/scripts/publish.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 IMAGE=${ACCOUNT}.dkr.ecr.ap-southeast-2.amazonaws.com/api-demo
 appImage="${IMAGE:=api-demo}:${BUILDKITE_BUILD_NUMBER:=latest}"
 
-eval $(aws ecr get-login --registry-ids ${ACCOUNT} --region ap-southeast-2 --no-include-email)
+eval $(aws ecr get-login --registry-ids ${ACCOUNT} --region ap-southeast-2 --no-include-email --profile personal_prod)
 docker push $appImage
